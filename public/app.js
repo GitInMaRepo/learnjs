74d059f9cd997fb380ae487bd29b6b6afc567e94
GitInMaRepo <oliver.s.roth@gmail.com>
2018-05-16 21:41:48 +0200

    added data binding

--- public/app.js
+++ public/app.js
@@ -1,9 +1,22 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problems = [
+    {
+        description: "Make this evaluate truthy",
+        code: "function problem() { return ___; }"
+    },
+    {
+        description: "Do some maths",
+        code: "function problem() { 6 * ___ === 42; }"
+    }
+]
+
 learnjs.problemView = function(parameter) {
-    var title = 'Problem ' + parameter + ' coming soon';
-    return $('<div class="problem-view">').text(title);
+    var problemNumber = parseInt(parameter);
+    var view = $('.templates .problems-view').clone();
+    view.find('.title').text('Problem #' + parameter);
+    return learnjs.applyBindings(view, learnjs.problems[problemNumber-1]);
 }
 
 learnjs.showView = function(hash) {
@@ -24,4 +37,11 @@ learnjs.appOnReady = function() {
         learnjs.showView(window.location.hash)
     }
     learnjs.showView(window.location.hash);
+}
+
+learnjs.applyBindings = function(element, data) {
+    for(var key in data) {
+        element.find('[data-name="'+key+'"]').text(data[key]);
+    }
+    return element;
 };
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,7 +1,7 @@
 describe('LearnJS', function () {
     it('can show the problem view', function () {
         learnjs.showView('#problem-1');
-        expect($('.view-container .problem-view').length).toEqual(1);
+        expect($('.view-container .problems-view').length).toEqual(1);
     });
 
     it('shows the landing page on an empty route/hash', function () {
@@ -31,7 +31,30 @@ describe('LearnJS', function () {
     describe('The problem view', function() {
         it('shows the problem number', function() {
             var view = learnjs.problemView('1');
-            expect(view.text()).toEqual('Problem 1 coming soon');
+            expect(view.find('.title').text()).toEqual('Problem #1');
+        });
+
+        it('has the fields to bind data to', function() {
+            var view = learnjs.problemView('1');
+            expect(view.find('[data-name="description"]').length).toEqual(1);
+            expect(view.find('[data-name="code"]').length).toEqual(1);
+        });
+
+        it('binds data to itself', function(){
+            spyOn(learnjs, 'applyBindings');
+            learnjs.problemView('1');
+            expect(learnjs.applyBindings).toHaveBeenCalled();
+        });
+    });
+
+    describe('The databinding applicator', function() {
+        it('sets the data to the bound elements', function() {
+            var view = $('.templates .problems-view').clone();
+            var result = learnjs.applyBindings(view, learnjs.problems[0]);
+            expect(result.find('[data-name="description"]').text())
+                .toEqual("Make this evaluate truthy");
+            expect(result.find('[data-name="code"]').text())
+                .toEqual("function problem() { return ___; }");
         });
     });
 });
